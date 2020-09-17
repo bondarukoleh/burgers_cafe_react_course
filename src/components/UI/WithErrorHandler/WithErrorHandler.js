bac7474979701cadfbd8af6693dbe1692864482d
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-09-18 01:04:19 +0200

    added interceptors removal

--- src/components/UI/WithErrorHandler/WithErrorHandler.js
+++ src/components/UI/WithErrorHandler/WithErrorHandler.js
@@ -4,35 +4,43 @@ import Error from "../Error/Error";
 
 function WithErrorHandler(WrappedComponent, axios) {
   return class extends Component {
-    state = {
-      error: false
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: false
+      };
     }
 
-    setAxios () {
-      axios.interceptors.request.use(request => {
+    setAxios = () => {
+      this.reqInterceptor = axios.interceptors.request.use(request => {
         this.setState({error: false});
         return request;
       }, error => {
         this.setState({error});
       });
 
-      axios.interceptors.response.use(response => {
+      this.resInterceptor = axios.interceptors.response.use(response => {
         return response;
       }, error => {
         this.setState({error});
       });
-    }
+    };
 
     errorConfirmed = () => {
       this.setState({error: false});
     };
 
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor)
+      axios.interceptors.response.eject(this.resInterceptor)
+    }
+
     render() {
-      this.setAxios()
+      this.setAxios();
       return (
         <React.Fragment>
           <Modal show={!!this.state.error} shadeClick={this.errorConfirmed}>
-            <Error error={this.state.error} errorConfirmed={this.errorConfirmed} />
+            <Error error={this.state.error} errorConfirmed={this.errorConfirmed}/>
           </Modal>
           <WrappedComponent {...this.props}/>
         </React.Fragment>
