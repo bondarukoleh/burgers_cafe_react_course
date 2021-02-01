04463f97ea67101da1f83ed7745b50af5b4543a9
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2021-02-01 23:45:27 +0100

    added automatic logout after token expires

--- src/store/actions/AuthActionCreator.js
+++ src/store/actions/AuthActionCreator.js
@@ -7,11 +7,12 @@ const loginUser = (userData, signIn) => async (dispatch) => {
       ? await signInRequest.post('',{returnSecureToken: true, ...userData})
       : await signUpRequest.post('',{returnSecureToken: true, email: userData.email, password: userData.password});
     if(result?.data) {
+      console.log(result);
       dispatch({
         type: Actions.userSignIn,
         payload: result.data
       });
-      // checkAuthExpirationTime(result.data.)
+      dispatch(setLogoutExpirationTime(result.data.expiresIn))
     } else {
       dispatch({
         type: Actions.errorOccurred,
@@ -26,16 +27,16 @@ const loginUser = (userData, signIn) => async (dispatch) => {
   }
 }
 
-const logoutUser = () => (dispatch) => {
+const logoutUser = (dispatch) => {
   dispatch({
     type: Actions.userSighOut
   })
 }
 
-const checkAuthExpirationTime = (expirationTime) => {
+const setLogoutExpirationTime = (expirationTime) => (dispatch) => {
   setTimeout(() => {
-    logoutUser();
-  }, expirationTime);
+    logoutUser(dispatch)
+  }, expirationTime * 1000);
 };
 
-export {loginUser, checkAuthExpirationTime, logoutUser}
+export {loginUser, setLogoutExpirationTime, logoutUser}
