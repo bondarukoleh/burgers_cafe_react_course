3c1b4717cdbd69df710dcd0fe26110eb69f2ab04
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2021-03-23 23:52:28 +0100

    continue with hooks

--- playground/hooks/src/components/Ingredients/Search.js
+++ playground/hooks/src/components/Ingredients/Search.js
@@ -1,22 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 import {request} from "../../api/request";
 
 const Search = React.memo(({onFilteredIngredients}) => {
-  const [searchValue, setSearch] = useState('')
+  const [searchValue, setSearch] = useState('');
+  const inputRef = useRef()
 
   useEffect(() => {
     const query = searchValue.length === 0 ? '' : `?indexOn="title"&&orderBy="title"&&equalTo="${searchValue}"`
-    request({path: `/ingredients.json${query}`})
-      .then(ingredientsDB => {
-        if(ingredientsDB && Object.keys(ingredientsDB).length) {
-          const formatTheIngredients = ([id, {title, amount}]) => ({id, title, amount});
-          onFilteredIngredients(Object.entries(ingredientsDB).map(formatTheIngredients));
-        }
-      })
-      .catch(err => console.log(`Something went wrong, couldn't get the ingredients`, err))
+    const timer = setTimeout(() => {
+      if(inputRef.current.value === searchValue)
+      request({path: `/ingredients.json${query}`})
+        .then(ingredientsDB => {
+          if(ingredientsDB && Object.keys(ingredientsDB).length) {
+            const formatTheIngredients = ([id, {title, amount}]) => ({id, title, amount});
+            onFilteredIngredients(Object.entries(ingredientsDB).map(formatTheIngredients));
+          }
+        })
+        .catch(err => console.log(`Something went wrong, couldn't get the ingredients`, err))
+    }, 500);
+    return () => clearTimeout(timer);
   }, [searchValue, onFilteredIngredients])
 
   return (
@@ -24,7 +29,7 @@ const Search = React.memo(({onFilteredIngredients}) => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" onChange={(e) => setSearch(e.target.value)} value={searchValue} />
+          <input ref={inputRef} type="text" onChange={(e) => setSearch(e.target.value)} value={searchValue} />
         </div>
       </Card>
     </section>
