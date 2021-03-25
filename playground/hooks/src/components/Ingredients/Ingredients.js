ad5e28c3fffd667248751fb5a5bc3542158627c1
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2021-03-25 19:42:45 +0100

    added more useReduser hooks

--- playground/hooks/src/components/Ingredients/Ingredients.js
+++ playground/hooks/src/components/Ingredients/Ingredients.js
@@ -1,73 +1,106 @@
-import React, {useState, useCallback, useReducer} from 'react';
+import React, {useCallback, useReducer} from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal'
 import {request} from '../../api/request';
 
+const IngredientActionTypes = {
+  set: 'SET',
+  add: 'ADD',
+  remove: 'REMOVE',
+}
+const ApiRequestActionTypes = {
+  sending: 'SENDING',
+  success: 'SUCCESS',
+  error: 'ERROR',
+  clear_error: 'CLEAR_ERROR',
+}
+
 const ingredientReducer = (currentIgredients, action) => {
   switch (action.type) {
-    case 'SET':
+    case IngredientActionTypes.set:
       return action.ingredients;
-    case 'ADD':
+    case IngredientActionTypes.add:
       return currentIgredients.concat(action.ingredient);
-    case 'REMOVE':
+    case  IngredientActionTypes.remove:
       return currentIgredients.filter((elem) => elem.id !== action.id);
     default:
       throw Error("Shouldn't get here in ingredientReducer!");
   }
 }
+const apiRequestReducer = (currentState, action) => {
+  switch (action.type) {
+    case ApiRequestActionTypes.sending:
+      return {loading: true, error: null};
+    case ApiRequestActionTypes.success:
+      return {...currentState, loading: false};
+    case ApiRequestActionTypes.error:
+      return {loading: false, error: action.error};
+    case ApiRequestActionTypes.clear_error:
+      return {...currentState, error: null};
+    default:
+      throw Error("Shouldn't get here in apiRequestReducer!");
+  }
+}
+
+const getDefaultIfEmpty = (err, message) => (err && err.message)
+  ? err.message
+  : `Sorry something went wrong. ${message}`;
 
 function Ingredients() {
   const [ingredients, dispatchIngredients] = useReducer(ingredientReducer, []);
-  /* With useState const [ingredients, setIngredients] = useState([]); */
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState({present: false, message: ''});
+  const [apiRequestState, dispatchRequestReducer] = useReducer(apiRequestReducer, {loading: false, error: null});
+  /* With useState
+   const [ingredients, setIngredients] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState({present: false, message: ''}); */
 
   const removeIngredient = (id) => {
-    setLoading(true);
+    dispatchRequestReducer({type: ApiRequestActionTypes.sending})
     request({path: `/ingredients/${id}.json`, method: 'DELETE'})
       .then((response) => {
-        setLoading(false);
-        dispatchIngredients({type: 'REMOVE', id: id})
+        dispatchRequestReducer({type: ApiRequestActionTypes.success})
+        dispatchIngredients({type: IngredientActionTypes.remove, id: id})
       })
       .catch(err => {
-        setLoading(false);
-        setError({present: true, message: err.message});
+        dispatchRequestReducer({type: ApiRequestActionTypes.error, error: getDefaultIfEmpty(err.message, 'Remove ingredient.') });
       })
   };
 
   const addIngredient = async (ing) => {
-    setLoading(true);
+    dispatchRequestReducer({type: ApiRequestActionTypes.sending});
     request({
       path: '/ingredients.json',
       method: 'POST',
       body: JSON.stringify(ing)
     })
       .then(res => {
-        setLoading(false);
-        dispatchIngredients({type: 'ADD', ingredient: {...ing, id: res.name}})
+        dispatchRequestReducer({type: ApiRequestActionTypes.success})
+        dispatchIngredients({type: IngredientActionTypes.add, ingredient: {...ing, id: res.name}})
         /*setIngredients(ingredients.concat({...ing, id: res.name}));*/
       })
       .catch(err => {
-        setLoading(false);
-        setError({present: true, message: err.message});
+        dispatchRequestReducer({type: ApiRequestActionTypes.error, error: getDefaultIfEmpty(err)});
+        /* with use state:
+         setLoading(false);
+         setError({present: true, message: err.message}); */
       });
   };
 
   const setFilteredIngredients = useCallback((ingredients) => {
-    dispatchIngredients({type: 'SET', ingredients})
+    dispatchIngredients({type: IngredientActionTypes.set, ingredients})
     /* setIngredients(ingredients) */
   }, []);
 
   return (
     <div className="App">
-      <IngredientForm addIngredient={addIngredient} loading={loading}/>
+      <IngredientForm addIngredient={addIngredient} loading={apiRequestState.loading}/>
       <section>
         <Search onFilteredIngredients={setFilteredIngredients}/>
         <IngredientList ingredients={ingredients} onRemoveItem={removeIngredient}/>
-        {error.present && <ErrorModal onClose={() => setError({present: false})}>
-          <p>{error?.message?.length ? error.message : 'Something went wrong, sorry about that'}</p>
+        {apiRequestState.error && <ErrorModal onClose={() => dispatchRequestReducer({type: ApiRequestActionTypes.clear_error})}>
+          <p>{apiRequestState.error}</p>
         </ErrorModal>}
       </section>
     </div>
