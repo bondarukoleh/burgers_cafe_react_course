1558bb01d3730adc13be7d1680f259a8b3b492d3
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2021-04-09 00:09:10 +0200

    deploy fixes

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+// Please rename this file without dist and add an Firebase API Token.
+const {FIREBASE_API_TOKEN} = process.env;
+if(!FIREBASE_API_TOKEN) {
+  throw new Error('FIREBASE_API_TOKEN shou;d be provided!')
+}
+const firebaseAPIToken = FIREBASE_API_TOKEN;
+
+export {firebaseAPIToken}
