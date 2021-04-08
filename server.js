42e96b51ec589d430767c2c1a736e9357df9c411
bondaruk9000@gmail.com <bondaruk9000@gmail.com>
2021-04-08 23:53:42 +0200

    added server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const buildPath = path.resolve(process.cwd(), 'build');
+const indexHtmlPath = path.join(buildPath, 'index.html');
+const port = process.env.PORT || 8080;
+
+
+const app = express();
+app.use(express.static(buildPath));
+app.get('*', (req, res) => {
+   res.sendFile(indexHtmlPath);
+});
+
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
